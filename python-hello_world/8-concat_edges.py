bdd748ca760bad346a2d1482c6f2dfce10818710
jaylenperez <jaylenestella24@gmail.com>
2024-04-30 16:48:11 -0500

    concatenated edges without using loops, conditionals, or creating new variables

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-text = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(text[49:84])
+str = "Python is an interpreted, interactive, object-oriented programming\
+    language that combines remarkable power with very clear syntax"
+print(str[39:59] + str[0:6] + str[59:60])
