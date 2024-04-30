bad97d546fb31ebec881fbdc4b740cea234ed26e
jaylenperez <jaylenestella24@gmail.com>
2024-04-30 11:08:20 -0500

    replaced str variable with text

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
+text = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[49:85])
+print(text[49:85])
