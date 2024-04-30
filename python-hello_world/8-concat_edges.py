59525088ee5ba129fe887359dc12bac02b0ddddd
jaylenperez <jaylenestella24@gmail.com>
2024-04-30 17:22:06 -0500

    use find method to locate the starting positions of the substrings

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
     language that combines remarkable power with very clear syntax"
-print(str[39:59] + str[:6] + str[59:60])
+print(str[str.find("object"):str.find("object")+14] + str[str.find("Python"):str.find("Python")+6] + str[str.find("with"):])
