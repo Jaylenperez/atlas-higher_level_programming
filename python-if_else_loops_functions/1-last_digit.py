73034f7673c85e8478e6fb7eae1b04f9d590194b
jaylenperez <jaylenestella24@gmail.com>
2024-05-01 21:00:14 -0500

    implemented absolute value of number before computing the last digit, then adjust the message based on it its negative

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,12 +6,14 @@ number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 
 # Determine the message based on the last digit
-if last_digit > 5:
-    message = "and is greater than 5"
-elif last_digit == 0:
-    message = "and is 0"
-else:
+if number < 0:
     message = "and is less than 6 and not 0"
-
+else:
+    if last_digit > 5:
+        message = "and is greater than 5"
+    elif last_digit == 0:
+        message = "and is 0"
+    else:
+        message = "and is less than 6 and not 0"
 # Print the result
 print(f"Last digit of {number} is {last_digit} {message}")
