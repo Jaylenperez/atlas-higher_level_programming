88f070735fa1bc3ea00d4871dab2b26494eeec25
jaylenperez <jaylenestella24@gmail.com>
2024-05-02 12:27:38 -0500

    added if else statement to ensure the last digit does not have a comma after it

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,9 @@
 
 for i in range(10):
     for j in range(i+1, 10):
-        print("{:02d}, ".format(i*10 + j), end="")
+        # Check if this is the last combination
+        if i == 8 and j == 9:
+            print("{:02d}".format(i*10 + j))
+        else:
+            print("{:02d}, ".format(i*10 + j), end="")
+
