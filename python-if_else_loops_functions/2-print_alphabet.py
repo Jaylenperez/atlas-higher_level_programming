b26597b5129eff7aed0684ffe7bc3eb74d3ba66e
jaylenperez <jaylenestella24@gmail.com>
2024-05-01 22:05:28 -0500

    used end parameter instead

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 
-import sys
-
-print(''.join(['{}'.format(chr(i)) for i in range(ord('a'), ord('z')+1)]))
+print(''.join(['{}'.format(chr(i)) for i in range(ord('a'), ord('z')+1)]), end='')
