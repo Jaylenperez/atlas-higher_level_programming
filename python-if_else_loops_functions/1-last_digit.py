ecab4fe54b3af69fbcb32aefa9a7b0ad42d89257
jaylenperez <jaylenestella24@gmail.com>
2024-05-01 21:06:06 -0500

    ensure last digit is always positive when printing

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,14 +6,15 @@ number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 
 # Determine the message based on the last digit
-if number < 0:
-    message = "and is less than 6 and not 0"
+if last_digit > 5:
+    message = "and is greater than 5"
+elif last_digit == 0:
+    message = "and is 0"
 else:
-    if last_digit > 5:
-        message = "and is greater than 5"
-    elif last_digit == 0:
-        message = "and is 0"
-    else:
-        message = "and is less than 6 and not 0"
-# Print the result
+    message = "and is less than 6 and not 0"
+
+# Print the result, including the sign of the last digit if original number was negative
+if number < 0:
+    last_digit = -last_digit
+
 print(f"Last digit of {number} is {last_digit} {message}")
