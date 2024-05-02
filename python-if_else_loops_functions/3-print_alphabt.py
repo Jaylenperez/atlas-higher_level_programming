4215007d0446d9c41841f97e340ee7d1027e112e
jaylenperez <jaylenestella24@gmail.com>
2024-05-01 22:22:24 -0500

    use list comprehension to generate a string with placeholders

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 # Print all lowercase letters except 'q' and 'e'
-print("".join([chr(i) for i in range(ord('a'), ord('z')+1) if chr(i) not in ['q', 'e']]))
+print("".join("{}" for _ in range(ord('a'), ord('z')+1) if chr(_) not in ['q', 'e']).format(*[chr(_) for _ in range(ord('a'), ord('z')+1) if chr(_) not in ['q', 'e']]))
