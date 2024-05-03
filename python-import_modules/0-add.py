3f4a285a97d3cb48ecd20ef2bb3133dd835b5ed2
jaylenperez <jaylenestella24@gmail.com>
2024-05-03 18:53:47 -0500

    modify import statement to import the add function directly

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 a = 1
 b = 2
-add_0 = __import__("add_0")
-result = add_0.add(a, b)
+from add_0 import add
+result = add(a, b)
 
 if __name__== "__main__":
     print("{} + {} = {}".format(a, b, result))
