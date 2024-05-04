cc5efbc18c21666f382c851fd80e419cb3d8768a
jaylenperez <jaylenestella24@gmail.com>
2024-05-04 10:57:35 -0500

    added blank lines on 6 and 16

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,7 @@
 import importlib.util
 import sys
 
+
 def main():
     spec = importlib.util.spec_from_file_location("hidden_4", "hidden_4.pyc")
     hidden_4 = importlib.util.module_from_spec(spec)
@@ -11,6 +12,7 @@ def main():
     for name in dir(hidden_4):
         if not name.startswith("__"):
             print(name)
+
 
 if __name__ == "__main__":
     main()
