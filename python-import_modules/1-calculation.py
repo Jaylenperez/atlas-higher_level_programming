e0f94ae4460c320ea4f8eac9ad43ad04971cc9d1
jaylenperez <jaylenestella24@gmail.com>
2024-05-03 22:15:13 -0500

    only use calculator_1 once

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
+import importlib
+
 def main():
     a = 10
     b = 5
 
-    import calculator_1  # Import calculator_1 within main()
+    # Dynamically import calculator_1 within main()
+    calculator_1 = importlib.import_module('calculator_1')
 
     output = "{} + {} = {}\n".format(a, b, calculator_1.add(a, b))
     output += "{} - {} = {}\n".format(a, b, calculator_1.sub(a, b))
