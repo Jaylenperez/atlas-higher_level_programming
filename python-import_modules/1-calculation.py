da223c1474182c83ffd99cfd2db9897bc02f4c2b
jaylenperez <jaylenestella24@gmail.com>
2024-05-03 21:58:19 -0500

    ensure main function is executed only when the script is run directly

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 import calculator_1
 
-a = 10
-b = 5
+def main():
+    a = 10
+    b = 5
 
-output = "{} + {} = {}\n".format(a, b, calculator_1.add(a, b))
-output += "{} - {} = {}\n".format(a, b, calculator_1.sub(a, b))
-output += "{} * {} = {}\n".format(a, b, calculator_1.mul(a, b))
-output += "{} / {} = {}".format(a, b, calculator_1.div(a, b))
+    output = "{} + {} = {}\n".format(a, b, calculator_1.add(a, b))
+    output += "{} - {} = {}\n".format(a, b, calculator_1.sub(a, b))
+    output += "{} * {} = {}\n".format(a, b, calculator_1.mul(a, b))
+    output += "{} / {} = {}".format(a, b, calculator_1.div(a, b))
 
-print(output, end='')
+    print(output, end='')
+
+if __name__ == "__main__":
+    main()
