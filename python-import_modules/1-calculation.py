1aca784457bd559aaf604c828af067cafdf89674
jaylenperez <jaylenestella24@gmail.com>
2024-05-03 21:45:44 -0500

    use single format string for the output and only call print once

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,7 +4,10 @@ import calculator_1
 a = 10
 b = 5
 
-print(f"{a} + {b} = {calculator_1.add(a, b)}")
-print(f"{a} - {b} = {calculator_1.sub(a, b)}")
-print(f"{a} * {b} = {calculator_1.mul(a, b)}")
-print(f"{a} / {b} = {calculator_1.div(a, b)}")
+output = f"{a} + {b} = {calculator_1.add(a, b)}\n"
+output = f"{a} - {b} = {calculator_1.sub(a, b)}\n"
+output = f"{a} * {b} = {calculator_1.mul(a, b)}\n"
+output = f"{a} / {b} = {calculator_1.div(a, b)}"
+
+print(output, end='')
+
