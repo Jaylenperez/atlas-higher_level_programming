8e0ce2f35951973a0643f9e675a700f53f38a8e5
jaylenperez <jaylenestella24@gmail.com>
2024-05-04 12:56:58 -0500

    enumerate used to get both the index and the value

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,9 @@ def print_matrix_integer(matrix=[[]]):
     if not matrix or not matrix[0]:
         return
     for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=' ')
+        for i, num in enumerate(row):
+            if i < len(row) - 1:
+                print("{:d}".format(num), end=' ')
+            else:
+                print("{:d}".format(num))
         print()
