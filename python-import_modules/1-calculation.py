324871b7e69ce8d9023312e44e0c718cfe1dfb6f
jaylenperez <jaylenestella24@gmail.com>
2024-05-03 21:55:36 -0500

    use format method

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,10 +4,9 @@ import calculator_1
 a = 10
 b = 5
 
-output = f"{a} + {b} = {calculator_1.add(a, b)}\n"
-output += f"{a} - {b} = {calculator_1.sub(a, b)}\n"
-output += f"{a} * {b} = {calculator_1.mul(a, b)}\n"
-output += f"{a} / {b} = {calculator_1.div(a, b)}"
+output = "{} + {} = {}\n".format(a, b, calculator_1.add(a, b))
+output += "{} - {} = {}\n".format(a, b, calculator_1.sub(a, b))
+output += "{} * {} = {}\n".format(a, b, calculator_1.mul(a, b))
+output += "{} / {} = {}".format(a, b, calculator_1.div(a, b))
 
 print(output, end='')
-
