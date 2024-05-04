52ce3cf43e8e440630aad258cbb1971ede34947a
jaylenperez <jaylenestella24@gmail.com>
2024-05-03 21:47:56 -0500

    use +=

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -5,9 +5,9 @@ a = 10
 b = 5
 
 output = f"{a} + {b} = {calculator_1.add(a, b)}\n"
-output = f"{a} - {b} = {calculator_1.sub(a, b)}\n"
-output = f"{a} * {b} = {calculator_1.mul(a, b)}\n"
-output = f"{a} / {b} = {calculator_1.div(a, b)}"
+output += f"{a} - {b} = {calculator_1.sub(a, b)}\n"
+output += f"{a} * {b} = {calculator_1.mul(a, b)}\n"
+output += f"{a} / {b} = {calculator_1.div(a, b)}"
 
 print(output, end='')
 
