be6c1cf44ed0350f61fdb8daa7091e2b0336c57b
jaylenperez <jaylenestella24@gmail.com>
2024-05-04 21:08:48 -0500

    added blank lines

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -7,11 +7,13 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     return (a + c, b + d)
 
+
 if __name__ == "__main__":
     tuple_a = (1, 89)
     tuple_b = (88, 11)
     new_tuple = add_tuple(tuple_a, tuple_b)
     print(new_tuple)
+
 
     print(add_tuple(tuple_a, (1, )))
     print(add_tuple(tuple_a, ()))
