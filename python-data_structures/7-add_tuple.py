ff52eadc3b96f420b167910f59c1c9827df0df57
jaylenperez <jaylenestella24@gmail.com>
2024-05-04 21:07:59 -0500

    created function that adds 2 tuples

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = tuple_a[0] if len(tuple_a) >= 1 else 0
+    b = tuple_a[1] if len(tuple_a) >= 2 else 0
+    c = tuple_b[0] if len(tuple_b) >= 1 else 0
+    d = tuple_b[1] if len(tuple_b) >= 2 else 0
+
+    return (a + c, b + d)
+
+if __name__ == "__main__":
+    tuple_a = (1, 89)
+    tuple_b = (88, 11)
+    new_tuple = add_tuple(tuple_a, tuple_b)
+    print(new_tuple)
+
+    print(add_tuple(tuple_a, (1, )))
+    print(add_tuple(tuple_a, ()))
