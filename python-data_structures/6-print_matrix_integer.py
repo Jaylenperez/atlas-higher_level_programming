b8a289339208199aec25fcc96239f4e4bed4bb9c
jaylenperez <jaylenestella24@gmail.com>
2024-05-04 20:51:59 -0500

    ensure that space is only printed after an element if its not the last element in the row

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
-    if not matrix or not matrix[0]:
-        return
-    for i, row in enumerate(matrix):
-        for num in row:
-            print("{:d}".format(num), end=' ')
-        # Print a newline character only if it's not the last row
-        if i < len(matrix) - 1:
-            print()
+    for row in matrix:
+        for i in range(len(row)):
+            print("{:d}".format(row[i]), end="")
+            if i != len(row) - 1:
+                print(" ", end="")
+        print()
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+if __name__ == "__main__":
+    print_matrix_integer(matrix)
+    print("--")
+    print_matrix_integer()
