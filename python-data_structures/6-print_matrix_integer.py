8e67dbab451276d67ed8ab376ee08e71c17e2550
jaylenperez <jaylenestella24@gmail.com>
2024-05-04 20:53:39 -0500

    add blank lines to 2 11 and 18

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in range(len(row)):
@@ -7,11 +8,13 @@ def print_matrix_integer(matrix=[[]]):
                 print(" ", end="")
         print()
 
+
 matrix = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ]
+
 
 if __name__ == "__main__":
     print_matrix_integer(matrix)
