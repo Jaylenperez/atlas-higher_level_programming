bbf7b1001a06888c8a32ff72538f83b0fac77cd7
jaylenperez <jaylenestella24@gmail.com>
2024-05-05 17:13:13 -0500

    removed blank lines and added new line to end of file

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -3,12 +3,12 @@
 def update_dictionary(a_dictionary, key, value):
     """
     Updates a dictionary by adding or replacing a key/value pair.
-    
+
     Args:
     a_dictionary (dict): The dictionary to update.
     key (str): The key to add or update.
     value: The value to set for the key.
-    
+
     Returns:
     dict: The updated dictionary.
     """
