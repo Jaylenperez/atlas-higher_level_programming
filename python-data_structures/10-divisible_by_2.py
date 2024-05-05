d02427d1da44dd526c56edead808221193aa0fbf
jaylenperez <jaylenestella24@gmail.com>
2024-05-04 22:07:29 -0500

    moved blank line to 10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -7,10 +7,10 @@ def divisible_by_2(my_list=[]):
 
     return result
 
+
 if __name__ == "__main__":
     my_list = [0, 1, 2, 3, 4, 5, 6]
     list_result = divisible_by_2(my_list)
-
 
     i = 0
     while i < len(list_result):
