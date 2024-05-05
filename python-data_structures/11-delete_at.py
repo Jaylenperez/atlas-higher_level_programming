434ef2234a51998e3d05d836392de85ff4691ee0
jaylenperez <jaylenestella24@gmail.com>
2024-05-04 23:47:38 -0500

    print modified list instead of original

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
-        return [elem for elem in my_list]  # Create a copy of the original list using list comprehension
+        return my_list[:]  # Create a copy of the original list
 
     return my_list[:idx] + my_list[idx + 1:]
 
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     idx = 3
     new_list = delete_at(my_list, idx)
     print(new_list)
-    print(my_list)
+    print(new_list)  # Print the modified list instead of the original list
