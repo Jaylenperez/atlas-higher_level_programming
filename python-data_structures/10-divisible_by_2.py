d618a6c0afca6cb9f7566ae44ba08c08d5fff743
jaylenperez <jaylenestella24@gmail.com>
2024-05-04 22:06:40 -0500

    added new line to end of file

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -4,12 +4,13 @@ def divisible_by_2(my_list=[]):
 
     for num in my_list:
         result.append(num % 2 == 0)
-    
+
     return result
 
 if __name__ == "__main__":
     my_list = [0, 1, 2, 3, 4, 5, 6]
     list_result = divisible_by_2(my_list)
+
 
     i = 0
     while i < len(list_result):
