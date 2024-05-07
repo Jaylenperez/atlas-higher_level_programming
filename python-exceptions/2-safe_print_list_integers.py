b46ab25c6a52a7ca7dab038b9bc69bcf010acd08
jaylenperez <jaylenestella24@gmail.com>
2024-05-06 22:21:27 -0500

    explicitly catches IndexError separetly

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    # Initialize counter for the number of integers printed
+    count = 0
+    # Iterate over the list up to the specified number of elements or the end of the list
+    for i in range(x):
+        try:
+            # Attempt to format the element as an integer
+            print("{:d}".format(my_list[i]), end="")
+            # If successful, increment the counter
+            count += 1
+        except (ValueError, TypeError):
+            # If a ValueError or TypeError is raised, skip the element silently
+            pass
+        except IndexError:
+            # If an IndexError is raised (meaning we've reached the end of the list), break the loop
+            break
+    # Print a newline to ensure the output is properly formatted
+    print()
+    # Return the actual number of integers printed
+    return count
