d36251c0f3a6058ece3116e581a6397eacfe118d
jaylenperez <jaylenestella24@gmail.com>
2024-05-06 21:52:19 -0500

    created function that prints an integer with given format

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+def safe_print_integer(value):
+    try:
+        # Attempt to format the value as an integer and print it
+        print("{:d}".format(value))
+        # If successful, return True
+        return True
+    except ValueError:
+        # If a ValueError occurs (meaning the value couldn't be formatted as an integer), return False
+        return False
