f0165d234c976d595beaad0d9e35593e057a5f88
jaylenperez <jaylenestella24@gmail.com>
2024-05-10 23:31:55 -0500

    documented module

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+
+"""
+This module defines a simple Square class.
+
+The Square class is currently empty, serving as a placeholder for future implementation of square-related functionality.
+"""
+
 class Square:
     pass
