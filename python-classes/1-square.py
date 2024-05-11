7e9713caf3cb5e392f44080bf95901b78f15df33
jaylenperez <jaylenestella24@gmail.com>
2024-05-11 00:20:25 -0500

    document module and class

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
+
+"""
+This module defines a Square class for geometric calculations.
+"""
+
 class Square:
+    """
+    Represents a square with a given size.
+    
+    Attributes:
+        size (int): The length of a side of the square.
+    """
+
     def __init__(self, size):
+        """
+        Initializes a new Square instance.
+        
+        Args:
+            size (int): The length of a side of the square.
+        """
         self.Square__size = size
