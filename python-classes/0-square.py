a4a75fed9ccd5d2a6b75d323a67d35fbb056955c
jaylenperez <jaylenestella24@gmail.com>
2024-05-10 23:49:25 -0500

    added blank line 19

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -8,7 +8,6 @@ implementation of square-related functionality.
 """
 
 class Square:
-
     """
     A class representing a square.
 
@@ -16,3 +15,4 @@ class Square:
     related to squares.
     """
     pass
+
