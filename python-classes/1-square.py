b14bb6e77f842199220fde880697ce5380eb2d6a
jaylenperez <jaylenestella24@gmail.com>
2024-05-11 09:52:16 -0500

    removed blank lines

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,9 +5,10 @@ This module defines a Square class for geometric calculations.
 """
 
 class Square:
+
     """
     Represents a square with a given size.
-    
+
     Attributes:
         size (int): The length of a side of the square.
     """
@@ -15,7 +16,7 @@ class Square:
     def __init__(self, size):
         """
         Initializes a new Square instance.
-        
+
         Args:
             size (int): The length of a side of the square.
         """
