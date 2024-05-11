589b4c352b58efcc7787a5d545ae7ca25f05b5ea
jaylenperez <jaylenestella24@gmail.com>
2024-05-11 10:28:57 -0500

    removed blank lines and added line to 7

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -4,10 +4,11 @@
 This module defines a Square class for geometric calculations.
 """
 
+
 class Square:
     """
     Represents a square with a given size.
-    
+
     Attributes:
         _Square__size (int): The length of a side of the square.
     """
@@ -15,7 +16,7 @@ class Square:
     def __init__(self, size=0):
         """
         Initializes a new Square instance.
-        
+
         Args:
             size (int, optional): The length of a side of the square. Defaults to 0.
         """
@@ -24,11 +25,11 @@ class Square:
         if size < 0:
             raise ValueError("size must be an integer")
         self._Square__size = size
-    
+
     def area(self):
         """
         Returns the area of the square.
-        
+
         Returns:
             int: The area of the square.
         """
