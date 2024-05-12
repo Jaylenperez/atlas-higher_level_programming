c4470051b60825c657c368e2bf48a07fc63dc3ec
jaylenperez <jaylenestella24@gmail.com>
2024-05-12 16:22:37 -0500

    converted floats to int and added integer import to test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -17,8 +17,14 @@ def add_integer(a, b=98):
     Returns:
         int: The sum of a and b.
     """
-    if ((not isinstance(a, int) and not isinstance(a, float))):
+
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    
+    if not isinstance(a, int):
         raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
+    if not isinstance(b, int):
         raise TypeError("b must be an integer")
-    return (int(a) + int(b))
+    return a + b
