94fcb9ccd631fc8c2b6987fc2acf8cf3eae92d90
jaylenperez <jaylenestella24@gmail.com>
2024-05-13 14:56:54 -0500

    removed whitespacec

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """A module for printing a person's name."""
 
+
 def say_my_name(first_name, last_name=""):
     """Prints a person's name.
-    
+
     Args:
         first_name (str): The first name of the person.
         last_name (str, optional): The last name of the person. Defaults to "".
-        
+
     Raises:
         TypeError: If either first_name or last_name is not a string.
-        
+
     Examples:
         >>> say_my_name("John", "Doe")
         My name is John Doe
