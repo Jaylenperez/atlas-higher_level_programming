423cfd8c87cee4cd234a7d54d6025fc407b2101b
jaylenperez <jaylenestella24@gmail.com>
2024-05-20 15:58:30 -0500

    Created an empty readme and wrote a function that returns the list of available attributes and methods of an object

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+    """
+    This function takes an object as input and returns a list of its attributes and methods.
+    
+    Parameters:
+    obj (object): The object whose attributes and methods are to be listed.
+    
+    Returns:
+    list: A list of the object's attributes and methods.
+    """
+    return dir(obj)
+
+# Example usage
+if __name__ == "__main__":
+    lookup = __import__('0-lookup').lookup
+
+    class MyClass1(object):
+        pass
+
+    class MyClass2(object):
+        my_attr1 = 3
+        def my_meth(self):
+            pass
+
+    print(lookup(MyClass1))
+    print(lookup(MyClass2))
+    print(lookup(int))
