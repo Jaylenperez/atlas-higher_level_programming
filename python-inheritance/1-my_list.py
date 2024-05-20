67ff2a32c7e765042c6da75ba6de8af0c2c2348b
jaylenperez <jaylenestella24@gmail.com>
2024-05-20 16:54:47 -0500

    implement custom list class with sorting functionality

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,38 +1,9 @@
 #!/usr/bin/python3
+"""This module inherits from the list class"""
 
-"""
-This module defines a custom list class, 'MyList', which extends Python's
-built-in class.
-"""
 
 class MyList(list):
-    """
-    A subclass of list that overrides the default behavior to include a method for printing the list sorted in ascending order.
-    """
-
+    """A class that inherits from list"""
     def print_sorted(self):
-        """
-        Prints the list sorted in ascending order.
-        
-        This method iterates over the list, sorts it, and then prints it.
-        """
-        # Sort the list in place
-        self.sort(reverse=True)
-
-        # Print the sorted list
-        print(self)
-
-
-# Example usage
-if __name__ == "__main__":
-    MyList = __import__('1-my_list').MyList
-
-    my_list = MyList()
-    my_list.append(1)
-    my_list.append(4)
-    my_list.append(2)
-    my_list.append(3)
-    my_list.append(5)
-    print(my_list)
-    my_list.print_sorted()
-    print(my_list)
+        """print a sorted list"""
+        print(sorted(self))
