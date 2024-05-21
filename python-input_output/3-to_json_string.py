20959ba908aa6c53aa6a2e535694c67136232800
jaylenperez <jaylenestella24@gmail.com>
2024-05-21 15:43:28 -0500

    condensed line 10

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -7,6 +7,6 @@ import json
 def to_json_string(my_obj):
     """Returns the JSON representation of a string object"""
 
-    # Use the json.dumps() function to convert the input object to JSON formatted string.
+    # json.dumps() func to convert the input object to JSON formatted string.
     # json.dumps() takes the object and returns a stirng in JSON format.
     return json.dumps(my_obj)
