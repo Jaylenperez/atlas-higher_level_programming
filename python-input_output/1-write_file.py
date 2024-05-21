eef3502ddf2276edc33832f3be8df19744ee0690
jaylenperez <jaylenestella24@gmail.com>
2024-05-21 14:55:20 -0500

    added newline to end of file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """module definition"""
 
+
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF8) and returns number of characters written"""
+    """Writes a string to a UTF8 text file"""
     # Open the file in write mode ('w') with UTF-8 encoding.
     # If the file doesn't exist, it will be created.
     # If it does exist, its content will be overwritten.
