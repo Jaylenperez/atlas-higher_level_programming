46f0f8ca2db7908507e577780756df6184c2ae7e
jaylenperez <jaylenestella24@gmail.com>
2024-05-23 16:25:59 -0500

    corrected < to <= on line 38

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -35,7 +35,7 @@ class Rectangle(Base): # Create Rectangle class that inherits from Base class
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
