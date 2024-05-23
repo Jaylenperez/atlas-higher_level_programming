5e980e1a521815fa35bd768c80d53fd22a504ea7
jaylenperez <jaylenestella24@gmail.com>
2024-05-23 16:15:07 -0500

    Updated the class Rectangle by adding validation of all setter methods and instantiation, id excluded

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -15,28 +15,52 @@ class Rectangle(Base): # Create Rectangle class that inherits from Base class
 
     @property # Property methods and setters are used to encapsulate access to the private instance variables.
     def width(self):
+        """Gets the value for width"""
         return self.__width
+
     @width.setter
     def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
     def height(self):
+        """Gets the value for height"""
         return self.__height
+
     @height.setter
     def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
     def x(self):
+        """Gets the value for x"""
         return self.__x
+
     @x.setter
     def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
+        """Gets the value for y"""
         return self.__y
+
     @y.setter
     def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
