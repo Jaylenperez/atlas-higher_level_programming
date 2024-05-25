9066b411bdd9ed91c0e61f8a7634df0f0e57bd9c
jaylenperez <jaylenestella24@gmail.com>
2024-05-25 11:15:04 -0500

    add comments for personal use

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -2,22 +2,22 @@
 """
 Module for square class
 """
-from models.rectangle import Rectangle
+from models.rectangle import Rectangle # Importing the Rectangle class for inheritance
 
 
-class Square(Rectangle):
+class Square(Rectangle): # Defining a square class inheriting from Rectangle
     """
     The Square class
     """
-    def __init__(self, size, x=0, y=0, id=None):
+    def __init__(self, size, x=0, y=0, id=None): # Constructor for creating a square instance
         """
         Square class constructor
         """
-        super().__init__(size, size, x, y, id)
+        super().__init__(size, size, x, y, id) # Calling the parent class constructor with size for both width and height
     
     def __str__(self):
         """
         Str representation
         """
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
-                                                 self.width)
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, # Using the format method to construct a string that includes the class name "Square"
+                                                 self.width) # followed by the instance's ID, x-coordinate, y-coordinate, and width (side length)
