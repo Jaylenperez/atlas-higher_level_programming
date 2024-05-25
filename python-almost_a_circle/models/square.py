6d6c0510be03634c73f50328fc14ad4433d30a54
jaylenperez <jaylenestella24@gmail.com>
2024-05-25 12:06:09 -0500

    Add def update(self, *args, **kwargs) to Square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -33,3 +33,33 @@ class Square(Rectangle): # Defining a square class inheriting from Rectangle
         """
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, # Using the format method to construct a string that includes the class name "Square"
                                                  self.width) # followed by the instance's ID, x-coordinate, y-coordinate, and width (side length)
+
+    def update(self, *args, **kwargs):
+        """
+        Update the Square.
+        """
+        if args and len(args) != 0: # If the positional argument that is passed is not empty
+            for count, arg in enumerate(args): # Iterate through the positional arguments
+                if count == 0: # Assign the first  positional argument to id
+                    self.id = arg
+                elif count == 1: # Assign the second positional argument to size
+                    self.size = arg
+                elif count == 2: # Assign the third positional argument to x
+                    self.x = arg
+                elif count == 3: # Assign the fourth positional argument to y
+                    self.y = arg
+                else: continue # Skip any additional positional arguments beyond four
+
+        elif len(kwargs) > 0: # If the positional argument passed is empty
+            for key, value in kwargs.items(): # Iterate through the keyword arguments
+                if key == "id": # Assign the 'id' keyword argument to self.id
+                    self.id = value
+                elif key == "size": # Assign the 'size' keyword argument to both width and height
+                    self.width = value
+                    self.height = value
+                elif key == "x": # Assign the 'x' keyword argument to self.x
+                    self.x = value
+                elif key == "y": # Assign the 'y' keyword argument to self.y
+                    self.y = value
+                else: # Break the loop if a non-matching keyword argument is encountered
+                    break
