684bba9c32aa5d3fccca4191cdd3aa18ce8bed5c
jaylenperez <jaylenestella24@gmail.com>
2024-05-28 17:59:16 -0500

    Updating comments for base module

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -23,9 +23,9 @@ class Base:
         load_from_file(cls): Loads a list of instances from a JSON file.
     """
 
-    __nb_objects = 0 # Define class private attribute called number of objects. Initialize to 0
+    __nb_objects = 0 # Initialize private attribute to keep track of number of instances created.
 
-    def __init__(self, id=None): # Define class constructor that takes self and optional parameter ID Initialied to None
+    def __init__(self, id=None): # Constructor method for the Base class
         """
         Initializes a new instance of the class. If an ID is provided, it is assigned to the instance;
         otherwise, a unique ID is generated based on the current count of instances.
@@ -36,7 +36,7 @@ class Base:
             Base.__nb_objects += 1 # If value is not provided we increment the private class attribute by 1
             self.id = Base.__nb_objects # assign unique ID to each instance that is created from the base model class starting from each new instance or object
 
-    @staticmethod # Serialize 
+    @staticmethod # Stacic method to convert a list of dictionaries into a JSON string. Serialize
     def to_json_string(list_dictionaries):
         """
         Serializes a list of dictionaries into a JSON string.
@@ -47,13 +47,13 @@ class Base:
         Returns:
             str: JSON string representation of the input list of dictionaries.
         """
-        if list_dictionaries is None or list_dictionaries == []:
+        if list_dictionaries is None or list_dictionaries == []: # Returning an empty JSON array string if the input is None or an empty list.
             return "[]"
-        to_json = json.dumps(list_dictionaries) # change pattern object to JSON format 
+        to_json = json.dumps(list_dictionaries) # Converting the list of dictionaries to a JSON formatted string 
 
         return to_json
     
-    @classmethod
+    @classmethod # Class method to save a list of objects to a JSON file
     def save_to_file(cls, list_objs):
         """
         Saves a list of objects to a JSON file.
@@ -61,18 +61,18 @@ class Base:
         Args:
             list_objs (list): List of objects to serialize and save to a file.
         """
-        file_name = "{}.json".format(cls.__name__)
+        file_name = "{}.json".format(cls.__name__) # Constructing the filename based on the class name.
 
-        with open(file_name, "w") as jsonfile:
+        with open(file_name, "w") as jsonfile: # Opening the file in write mode and writing the serialized objects to it.
             if list_objs is None:
                 jsonfile.write("[]")
             else:
                 list_dict = []
                 for obj in list_objs:
-                    list_dict.append(obj.to_dictionary())
+                    list_dict.append(obj.to_dictionary()) # Assuming each object has a to_dictionary() method
                 jsonfile.write(Base.to_json_string(list_dict))
 
-    def from_json_string(json_string): # Deserialize string
+    def from_json_string(json_string): # Deserialize JSON string into a Pythin object
         """
         Deserialize a JSON sting into a Python object.
 
@@ -84,9 +84,9 @@ class Base:
         """
         if json_string is None or json_string == "[]": # If json string is empty
             return [] # Return empty python list
-        return json.loads(json_string) # Else, we should 
+        return json.loads(json_string) # Deserializing the JSON string into a Python object
 
-    @classmethod
+    @classmethod # Class method to create an instance with all attributes already set using a dictionary
     def create(cls, **dictionary):
         """
         Creates an instance with all attributes already set using a dictionary.
@@ -106,7 +106,7 @@ class Base:
 
             return dummy
 
-    @classmethod # Responsible for loading a lsit of instances from a json file 
+    @classmethod # Responsible for loading a lsit of instances from a JSON file 
     def load_from_file(cls):
         """
         Loads a list of instances from a JSON file.
@@ -114,16 +114,16 @@ class Base:
         Returns:
             list: List of instances loaded from the file.
         """
-        file_name = "{}.json".format(cls.__name__)
+        file_name = "{}.json".format(cls.__name__) # Constructing the filename based on the class name
 
         try:
-            with open(file_name, "r") as jsonfile:
-                list_dicts = Base.from_json_string(jsonfile.read())
+            with open(file_name, "r") as jsonfile: # Attempting to open the file in read mode and reading its content.
+                list_dicts = Base.from_json_string(jsonfile.read()) # Deserializing the file content into a list of dictionaries
 
                 list_instances = []
 
-                for d in list_dicts:
+                for d in list_dicts: # Creating instances for each dictionary in the list and appending them to the result list
                     list_instances.append(cls.create(**d))
                 return list_instances
         except FileNotFoundError:
-            return []
+            return [] # Returning an empty list if the file does not exist.
