5f34f123057f1909f42aa0a4a8fc156421db2897
jaylenperez <jaylenestella24@gmail.com>
2024-06-18 13:37:34 -0500

    script that lists all cities from the database hbtn_0e_4_usa

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+This script lists all cities from database hbtn_0e_4_usa
+"""
+import MySQL as db
+from sys import argv
+
+if __name__ =="__main__": # Ensure script runs only if it is executed directly, not if it's imported as a module
+    """
+    Access the database to get the cities
+    from the database
+    """
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3]) # Establish connection to the MySQL database.
+    
+    with db_connect.cursor() as db_cursor: # Create cursor object using a context manager to ensure proper resource management
+        db_cursor.execute("SELECT cities.id, cities.name, states.name \
+                                FROM cities JOIN states ON cities.state_id \
+                                = states.id ORDER BY cities.id ASC") # Execute SQL query to select city and state info, joining cities and states tables
+        rows_selected = db_cursor.fetchall() # Fetch all the rows from the executed query
+
+    if rows_selected is not None: # Check if there are any rows selected
+        for row in rows_selected: # Iterate over the rows in the result set
+            print(row) # Print each row in the result set.
