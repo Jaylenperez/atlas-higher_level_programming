646d2c15850b0fe50cf389a3b3ca0da3c74348b3
jaylenperez <jaylenestella24@gmail.com>
2024-07-20 16:27:35 -0500

    added comments

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
+// Create an array of strings containing desired lines
 const lines = [
   'C is fun',
   'Python is cool',
   'JavaScript is amazing'
 ];
 
+// Use a for-of loop to iterate through each line in the array
 for (const line of lines) {
-  console.log(line);
+  console.log(line); // Print each line
 }
