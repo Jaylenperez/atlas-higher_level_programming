cacbfe0085c9adb5237d16391ee2cfb52b521e8c
jaylenperez <jaylenestella24@gmail.com>
2024-07-20 16:44:03 -0500

    ensure script prints nothing when negative number is entered.

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,11 +4,11 @@
 const numOccurrences = parseInt(process.argv[2]);
 
 // Check if the argument is a valid positive integer
-if (isNaN(numOccurrences) || numOccurrences <= 0) {
-    console.log("Missing number of occurrences");
-} else {
-    // Use a loop to print "C is fun" the specified # of times
-    for (let i = 0; i < numOccurrences; i++) {
-        console.log("C is fun");
-    }
+if (isNaN(numOccurrences)) {
+  console.log('Missing number of occurrences');
+} else if (numOccurrences > 0) {
+  // Use a loop to print "C is fun" the specified number of times
+  for (let i = 0; i < numOccurrences; i++) {
+    console.log('C is fun');
+  }
 }
