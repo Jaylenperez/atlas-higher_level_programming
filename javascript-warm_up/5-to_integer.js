1c6346fde107924ea113ed5a28fbcadd4ed12251
jaylenperez <jaylenestella24@gmail.com>
2024-07-20 16:13:48 -0500

    ran semistandard --fix

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@ const input = process.argv[2]; // Retrieve the first command-line argument and s
 const parsedInt = parseInt(input); // Attempt to convert the input to an integer and store the result in 'parsedInt'
 
 if (!isNaN(parsedInt)) { // Check if 'parsedInt' is a valid integer (not NaN)
-    console.log(`My number: ${parsedInt}`); // If valid, print the converted integer
+  console.log(`My number: ${parsedInt}`); // If valid, print the converted integer
 } else {
-    console.log('Not a number'); // Otherwise, print "Not a number"
+  console.log('Not a number'); // Otherwise, print "Not a number"
 }
