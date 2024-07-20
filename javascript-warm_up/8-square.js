6781b0e6d5d98b6fe81e01d23b82d0f999ed086f
jaylenperez <jaylenestella24@gmail.com>
2024-07-20 16:54:13 -0500

    semistandard --fix

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,23 +1,23 @@
 #!/usr/bin/node
 
-function printSquare(size) {
-    // Convert the input to an integer
-    const squareSize = parseInt(size);
-    
-    // Check if the conversion was successful
-    if (isNaN(squareSize)) {
-        console.log('Missing size');
-        return;
-    }
+function printSquare (size) {
+  // Convert the input to an integer
+  const squareSize = parseInt(size);
 
-    // Print the square
-    for (let i = 0; i < squareSize; i++) {
-        let row = '';
-        for (let j = 0; j < squareSize; j++) {
-            row += 'X';
-        }
-        console.log(row);
+  // Check if the conversion was successful
+  if (isNaN(squareSize)) {
+    console.log('Missing size');
+    return;
+  }
+
+  // Print the square
+  for (let i = 0; i < squareSize; i++) {
+    let row = '';
+    for (let j = 0; j < squareSize; j++) {
+      row += 'X';
     }
+    console.log(row);
+  }
 }
 
 // Get the first argument from the command line
