1510557a19a060cea9bbdd974c8967eddea3de0e
jaylenperez <jaylenestella24@gmail.com>
2024-07-20 17:12:25 -0500

    semistandard --fix

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 
-function factorial(n) {
-    // Base case: Factorial of NaN is 1
-    if (isNaN(n) || n < 0) {
-        return 1;
-    }
+function factorial (n) {
+  // Base case: Factorial of NaN is 1
+  if (isNaN(n) || n < 0) {
+    return 1;
+  }
 
-    // Recursive case: Compute factorial
-    if (n === 0) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
-    }
+  // Recursive case: Compute factorial
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
 
 // Get the first argument from the command line
