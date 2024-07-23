139b0ec39b5c04238e5fe430d0fd7efacc9a15d1
jaylenperez <jaylenestella24@gmail.com>
2024-07-23 17:44:33 -0500

    modify constructor to assign the value of h to this.height

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -3,7 +3,7 @@ class Rectangle {
   constructor (w, h) {
     this.width = w;
     // Check if height is not provided or is negative, then set it to undefined.
-    this.height = typeof h === 'number' && h > 0 ? h : undefined;
+    this.height = typeof h === 'number' ? h : undefined;
   }
 }
 
