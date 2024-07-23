8527bc7da4d93111f036c80dc8a0c61f2483cb42
jaylenperez <jaylenestella24@gmail.com>
2024-07-23 18:11:15 -0500

    add comments to 2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,17 @@
 #!/usr/bin/node
+// Define class named Rectangle
 class Rectangle {
+  // Constructor method that gets called when a new instance of Rectangle is created  
   constructor (w, h) {
+    // Check if both width (w) and height (h) are greater than or equal to 1
     if (w >= 1 && h >= 1) {
+      // If the condition is true, set the instance attributes width and height  
       this.width = w;
       this.height = h;
     }
+    // If either w or h is less than 1, do nothing, leaving the instance without width and height properties
   }
 }
+
+// Export the Rectangle class so it can be imported and used in other JavaScript files.
 module.exports = Rectangle;
