ea16739285d94b9a97e59142393e7f4f36069711
jaylenperez <jaylenestella24@gmail.com>
2024-07-23 18:17:56 -0500

    wrote a class that defines a rectangle

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // Define class named Rectangle
 class Rectangle {
-  // Constructor method that gets called when a new instance of Rectangle is created  
+  // Constructor method that gets called when a new instance of Rectangle is created
   constructor (w, h) {
     // Check if both width (w) and height (h) are greater than or equal to 1
     if (w >= 1 && h >= 1) {
-      // If the condition is true, set the instance attributes width and height  
+      // If the condition is true, set the instance attributes width and height
       this.width = w;
       this.height = h;
     }
