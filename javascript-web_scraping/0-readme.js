582fe6a71bfdd6987ed44080689642d8aedca291
jaylenperez <jaylenestella24@gmail.com>
2024-07-27 15:45:38 -0500

    semistandard --fix

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -8,11 +8,11 @@ const filePath = process.argv[2];
 
 // Read the file content in UTF-encoding
 fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        // Print the error object if an error occured
-        console.error(err);
-    } else {
-        // Print the content of the file
-        console.log(data);
-    }
+  if (err) {
+    // Print the error object if an error occured
+    console.error(err);
+  } else {
+    // Print the content of the file
+    console.log(data);
+  }
 });
