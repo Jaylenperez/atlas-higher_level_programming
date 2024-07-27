1e1801b8d269a69a9b0cfbf9104c933dd6c5b415
jaylenperez <jaylenestella24@gmail.com>
2024-07-27 15:44:45 -0500

    created a script that reads and prints the content of a file

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+// Import the 'fs' module to work with file system
+const fs = require('fs');
+
+// Get the file path from the command-line arguments
+const filePath = process.argv[2];
+
+// Read the file content in UTF-encoding
+fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+        // Print the error object if an error occured
+        console.error(err);
+    } else {
+        // Print the content of the file
+        console.log(data);
+    }
+});
