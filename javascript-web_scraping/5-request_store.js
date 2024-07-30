05c2621586510586233362623a6d8fbed5417e18
jaylenperez <jaylenestella24@gmail.com>
2024-07-30 11:05:42 -0500

    Created a script the gets the contents of a webpage and stores it in a file

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+// Get the URL and file path from command line arguments
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+// Make a request to the URL
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(`Error: ${error}`);
+    return;
+  }
+
+  // Write the body to the file with UTF-8 encoding
+  fs.writeFile(filePath, body, 'utf8', (err) => {
+    if (err) {
+      console.error(`Error writing to file: ${err}`);
+      return;
+    }
+    console.log(`Content saved to ${filePath}`);
+  });
+});
